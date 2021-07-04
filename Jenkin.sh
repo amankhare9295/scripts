f2aa81fb301714affa45b6a11c5ac63c632e0e7c
amankhare9295 <75713933+amankhare9295@users.noreply.github.com>
2021-07-04 21:43:41 +0530

    Create Jenkin.sh

---
+++ Jenkin.sh
@@ -0,0 +1,4 @@
+#!/bin/bash/
+wget https://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.68/bin/apache-tomcat-8.5.68.tar.gz
+tar -xvzf apache-tomcat-8.5.68.tar.gz
+wget https://get.jenkins.io/war-stable/2.289.2/jenkins.war
