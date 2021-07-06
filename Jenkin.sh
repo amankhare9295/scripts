ae06bb57331b18e6ffd42e82a201a5b60fb90105
amankhare9295 <75713933+amankhare9295@users.noreply.github.com>
2021-07-06 16:36:56 +0530

    Update Jenkin.sh

--- Jenkin.sh
+++ Jenkin.sh
@@ -2,3 +2,5 @@
 wget https://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.68/bin/apache-tomcat-8.5.68.tar.gz
 tar -xvzf apache-tomcat-8.5.68.tar.gz
 wget https://get.jenkins.io/war-stable/2.289.2/jenkins.war
+mv jenkins.war /apache-tomcat-8.5.68/webapps/
+sh /apache-tomcat-8.5.68/bin/startup.sh
